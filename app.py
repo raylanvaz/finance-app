7f809147b66e099016fc10298c68795037a19a3b
Raylan <raylan.vaz@gmail.com>
2025-02-22 17:59:53 -0800

    Added upload functionality

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Folder to save files
 
 @app.route('/')
 def home():
-    return "Hello World! This is my finance app!"
+    return render_template('upload.html')  # We'll create this next
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file uploaded!"
+    file = request.files['file']
+    if file.filename == '':
+        return "No file selected!"
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return "File uploaded!"
 
 if __name__ == '__main__':
+    os.makedirs('uploads', exist_ok=True)  # Create uploads folder
     app.run(debug=True)
